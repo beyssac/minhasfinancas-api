e00b7bebde7a808305ebe2d99e193f1bef89f730
Pedro Henrique <pedrohenrique.ufrj@gmail.com>
2021-05-13 12:38:11 -0300

    Corrigindo pacote config

--- src/main/java/com/udemy/cursospringbootreact/CursoSpringbootReactApplication.java
+++ src/main/java/com/udemy/cursospringbootreact/CursoSpringbootReactApplication.java
@@ -2,11 +2,10 @@ package com.udemy.cursospringbootreact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CursoSpringbootReactApplication{
+public class CursoSpringbootReactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringbootReactApplication.class, args);
