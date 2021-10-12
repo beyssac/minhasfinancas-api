f562d97d830f84f52f0b537f29d340cd768bb916
Pedro Henrique <pedrohenrique.ufrj@gmail.com>
2021-10-12 12:46:35 -0300

    CORS config + DTO repair

--- src/main/java/com/udemy/cursospringbootreact/config/SecurityConfiguration.java
+++ src/main/java/com/udemy/cursospringbootreact/config/SecurityConfiguration.java
@@ -1,7 +1,12 @@
 package com.udemy.cursospringbootreact.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.udemy.cursospringbootreact.api.JwtTokenFilter;
 import com.udemy.cursospringbootreact.service.JwtService;
@@ -58,4 +66,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		
 	}
 	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		
+		List<String> all = Arrays.asList("*");
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedMethods(all);
+		config.setAllowedOriginPatterns(all);
+		config.setAllowedHeaders(all);
+		config.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		CorsFilter corfilter = new CorsFilter(source);
+		FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<CorsFilter>(corfilter);
+		filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
+		return filter;		
+		
+	}
 }
